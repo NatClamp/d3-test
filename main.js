71c735bdd31a3040e042624910ba988a1c7213d1
NatClamp <nat.clamp@gmail.com>
2019-02-19 17:23:24 +0000

    Adding colour to bar chart based on data

--- main.js
+++ main.js
@@ -1,71 +1,23 @@
-// const d3 = require('d3');
-// const doc = d3.select(window.document);
-
-// const h = 350;
-// const w = 400;
-
-// monthlySales = [
-//   { month: 10, sales: 100 },
-//   { month: 20, sales: 130 },
-//   { month: 30, sales: 250 },
-//   { month: 40, sales: 300 },
-//   { month: 50, sales: 265 },
-//   { month: 60, sales: 225 },
-//   { month: 70, sales: 180 },
-//   { month: 80, sales: 120 },
-//   { month: 90, sales: 145 },
-//   { month: 100, sales: 130 },
-// ];
-
-// const lineFun = d3.svg
-//   .line()
-//   .x(d => d.month)
-//   .y(d => h - d.sales)
-//   .interpolate('linear');
-
-// const svg = d3
-//   .select('body')
-//   .append('svg')
-//   .attr({
-//     width: w,
-//     height: h,
-//   });
-
-// const viz = svg.append('path').attr({
-//   d: lineFun(monthlySales),
-//   stroke: 'purple',
-//   'stroke-width': 2,
-//   fill: 'none',
-// });
-
-// const w = 300;
-// const h = 100;
-// const padding = 2;
-// const dataset = [5, 10, 15, 20, 25];
-
-// const svg = d3
-//   .select('body')
-//   .append('svg')
-//   .attr('width', w)
-//   .attr('height', h);
-
-// svg
-//   .selectAll('rect')
-//   .data(dataset)
-//   .enter()
-//   .append('rect')
-//   .attr('x', function(d, i) {
-//     return i * (w / dataset.length);
-//   })
-//   .attr('y', function(d) {
-//     return h - d * 4;
-//   })
-//   .attr('width', w / dataset.lengh - padding)
-//   .attr('height', function(d) {
-//     return d * 4;
-//   });
+const w = 300;
+const h = 100;
+const padding = 2;
 
 const circleRadii = [40, 20, 10];
+const barchartDataset = [15, 10, 15, 20, 25, 30, 50];
+const monthlySales = [
+  { month: 10, sales: 100 },
+  { month: 20, sales: 130 },
+  { month: 30, sales: 250 },
+  { month: 40, sales: 300 },
+  { month: 50, sales: 265 },
+  { month: 60, sales: 225 },
+  { month: 70, sales: 180 },
+  { month: 80, sales: 120 },
+  { month: 90, sales: 145 },
+  { month: 100, sales: 130 },
+];
+
+// SVG circles coloured by their radii
 
 const svgSelection = d3
   .select('body')
@@ -90,3 +42,45 @@ const circleAttributes = circleSelection
     else returnColor = 'red';
     return returnColor;
   });
+
+// Barchart - simple
+
+const svg = d3
+  .select('body')
+  .append('svg')
+  .attr('width', w)
+  .attr('height', h);
+
+svg
+  .selectAll('rect')
+  .data(barchartDataset)
+  .enter()
+  .append('rect')
+  .attr('x', (d, i) => i * (w / barchartDataset.length))
+  .attr('y', d => h - d * 2)
+  .attr('width', w / barchartDataset.length - padding)
+  .attr('height', d => d * 2)
+  .style('fill', d => `rgb(${d * 10}, 0, 0)`);
+
+// line chart
+
+const lineFun = d3.svg
+  .line()
+  .x(d => d.month)
+  .y(d => h - d.sales)
+  .interpolate('linear');
+
+const lineSVG = d3
+  .select('body')
+  .append('svg')
+  .attr({
+    width: w,
+    height: h,
+  });
+
+const viz = lineSVG.append('path').attr({
+  d: lineFun(monthlySales),
+  stroke: 'purple',
+  'stroke-width': 2,
+  fill: 'none',
+});
